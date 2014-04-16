9811a5cb410fd0b84f2d4f8a915763fdb85d226b
that0n3guy <that0n3guy@46440.no-reply.drupal.org>
2014-04-16 16:56:47 -0500

    fix send file url to ckeditor

--- src/views/ckeditor4.php
+++ src/views/ckeditor4.php
@@ -23,10 +23,24 @@
 
     <!-- elFinder initialization (REQUIRED) -->
     <script type="text/javascript" charset="utf-8">
+        // Helper function to get parameters from the query string.
+        function getUrlParam(paramName) {
+            var reParam = new RegExp('(?:[\?&]|&amp;)' + paramName + '=([^&]+)', 'i') ;
+            var match = window.location.search.match(reParam) ;
+
+            return (match && match.length > 1) ? match[1] : '' ;
+        }
+
         $().ready(function() {
+            var funcNum = getUrlParam('CKEditorFuncNum');
+
             var elf = $('#elfinder').elfinder({
-                <?php if($locale){ echo "lang: '$locale',\n"; } ?>
-                url: '<?= URL::action('Barryvdh\Elfinder\ElfinderController@showConnector') ?>',  // connector URL
+                url : 'php/connector.php',
+                getFileCallback : function(file) {
+                    window.opener.CKEDITOR.tools.callFunction(funcNum, file);
+                    window.close();
+                },
+                resizable: false
             }).elfinder('instance');
         });
     </script>
@@ -35,28 +49,7 @@
 
 <!-- Element where elFinder will be created (REQUIRED) -->
 <div id="elfinder"></div>
-<script type="text/javascript" charset="utf-8">
-    // Helper function to get parameters from the query string.
-    function getUrlParam(paramName) {
-        var reParam = new RegExp('(?:[\?&]|&amp;)' + paramName + '=([^&]+)', 'i') ;
-        var match = window.location.search.match(reParam) ;
 
-        return (match && match.length > 1) ? match[1] : '' ;
-    }
-
-    $().ready(function() {
-        var funcNum = getUrlParam('CKEditorFuncNum');
-
-        var elf = $('#elfinder').elfinder({
-            url : 'php/connector.php',
-            getFileCallback : function(file) {
-                window.opener.CKEDITOR.tools.callFunction(funcNum, file);
-                window.close();
-            },
-            resizable: false
-        }).elfinder('instance');
-    });
-</script>
 
 </body>
 </html>
