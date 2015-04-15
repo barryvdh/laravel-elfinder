bd379536b3aa3478f8ab7b10ceee2783292f6d80
Barry vd. Heuvel <barryvdh@gmail.com>
2015-04-15 16:15:52 +0200

    Update elfinder.php

--- config/elfinder.php
+++ config/elfinder.php
@@ -40,7 +40,7 @@ return array(
 
     'route' => [
         'prefix' => 'elfinder',
-        'middleware' => 'auth', //Set to null to disable middleware filter
+      //  'middleware' => 'auth', //Set to null to disable middleware filter
     ],
 
     /*
